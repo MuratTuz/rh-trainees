76d7aa58c5117f1f3ea1275d1755963f840b8949
MuratTuz <murattuzlali@gmail.com>
2021-04-15 14:27:42 +0200

    setObjectByTypeName has been changed

--- services/setObjectByTypeName.js
+++ services/setObjectByTypeName.js
@@ -8,15 +8,14 @@ const setObjectByTypeName = (obj, typeName, typeValue, typeValidations) => {
     console.log(obj);
     if (Object.keys(obj).length === 0) {
         console.log('hii');
-        return { [typeName]: setObjectProperty(typeValue, typeValidations) }
+        let objTemp = setObjectProperty(typeValue, typeValidations);
+        return { [typeName]: objTemp }
     } else {
         // iterates over object keys
         for (var i in obj) {
             if (obj.hasOwnProperty(i)) {
-                var objTemp = setObjectByTypeName(obj[i], typeName, typeValue, typeValidations);
-                return { ...obj[i], objTemp }
-            } else {
-                return { [typeName]: setObjectProperty(typeValue, typeValidations) }
+                let objTemp = setObjectByTypeName(obj[i], typeName, typeValue, typeValidations);
+                return { ...obj[i], objTemp };
             }
         }
 
