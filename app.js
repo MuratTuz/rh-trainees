a1c3ab4111187db300909893ffea52c21c5bb660
MuratTuz <murattuzlali@gmail.com>
2021-04-19 09:57:48 +0200

    folder structure re-arranged

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var infomaniakRouter = require('./routes/infomaniakRouter');
+var infomaniakRouter = require('./src/routes/infomaniakRouter');
 
 var app = express();
 
